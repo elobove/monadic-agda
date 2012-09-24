7a24037a28e5d0e22674c79988e41146b9bd182b
Elisabet <elilobo.v@gmail.com>
2012-09-23 19:20:24 -0500

    Editing code

--- NonDet.hs
+++ NonDet.hs
@@ -1,42 +1,22 @@
+module NonDet where
+
+import Prelude hiding (Monad, return, fail)
+import MyMonad
+
 -- NONDETERMINISTIC COMPUTATIONS
--- Failure
-class Monad m => MonadFail m where
-  fail' :: m a
-
-skip :: Monad m => m ()
-skip = return ()
-
+-- Examples
 guard :: MonadFail m => Bool -> m ()
 guard True  = skip
-guard False = fail'
+guard False = fail
 
-assert :: MonadFail m => (a -> Bool) -> m a -> m a
+assert :: (a -> Bool) -> [a] -> [a]
 assert p mx = do {x <- mx; guard (p x); return x}
 
--- Choice
-class Monad m => MonadAlt m where
-  op :: m a -> m a -> m a
-  
--- Nondeterminism
-class (MonadFail m, MonadAlt m) => MonadNondet m where
-    
-{-
-instance Monad [] where
-  return x = [x]
-  xs >>= f = concat (map f xs)
--}
-
-instance MonadFail [] where
-  fail' = []
-  
-instance MonadAlt [] where
-  op = (++)
-  
 -- Permutations
-select :: MonadNondet m => [a] -> m(a,[a])
-select []     = fail'
-select (x:xs) = op (return (x,xs)) (do {(y,ys) <- select xs; return (y,x:ys)}) 
+select :: [a] -> [(a,[a])]
+select []     = fail
+select (x:xs) = sq (return (x,xs)) (do {(y,ys) <- select xs; return (y,x:ys)})
 
-perms :: MonadNondet m => [a] -> m [a]
+perms :: [a] -> [[a]]
 perms [] = return []
 perms xs = do {(y,ys) <- select xs; zs <- perms ys; return (y:zs)}
