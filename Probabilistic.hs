9c2f0a33206fd8df3695a70526e44944fb491204
Elisabet <elilobo.v@gmail.com>
2012-11-06 01:58:43 -0500

    Cleaning code

--- Probabilistic.hs
+++ Probabilistic.hs
@@ -8,31 +8,31 @@ import System.Environment
 main :: IO()
 main = do
     g <- newStdGen
-    putStrLn "¿Cual es tu estrategia?"
+    putStrLn "What is your strategy?"
     strategy <- getLine
     if strategy == "switch"
-      then print (play g switch)
-           else if strategy == "stick"
-                   then print (play g switch)
-                        else putStrLn "Eso no es una estrategia! ¬¬"
+      then print (play2 g switch)
+      else if strategy == "stick"
+           then print (play2 g stick)
+           else putStrLn "This is not a strategy! YOU LOSE!!!"
 
 -- Standard normal distribution
 std :: Double -> Double
-std x = (exp((-x^2)/2)/sqrt(4*pi))
+std x = exp((-x^2)/2)/sqrt(4*pi)
 
 class Monad m => MonadProb m where
   choice :: (MonadProb m) => Double -> Double -> m b -> m b -> m b
 
 instance MonadProb [] where
-  choice p real x y = if p < (std real) then y else x
+  choice p real x y = if p < std real then y else x
 
 length1 :: Fractional b => [a] -> b
-length1 [] = 0.0
-length1 (x:xs) = 1.0 + (length1 xs)
+length1 = foldr (\x -> (+) 1.0) 0.0
 
 uniform :: [a] -> Double -> [a]
 uniform [x] _ = return x
-uniform (x:xs) real = choice (1/length1 (x:xs)) real (return x) (uniform xs real)
+uniform (x:xs) real = 
+  choice (1/length1 (x:xs)) real (return x) (uniform xs real)
 
 -- THE MONTY HALL PROBLEM
 data Door = A | B | C deriving (Eq, Show)
@@ -42,19 +42,20 @@ doors = [A,B,C]
 
 -- Hiding the car behind one door ramdonly
 hide :: Double -> [Door]
-hide x = uniform doors x
+hide = uniform doors
 
 -- Selecting one door ramdonly
 pick :: Double -> [Door]
-pick x = uniform doors x
+pick = uniform doors
 
+-- Relative complement
 (\/) :: Eq a => [a] -> [a] -> [a]
 [] \/ _ = []
 x \/ y = [a | a <- x, a `notElem` y]
 
 -- Opening one door to reveal a goat
 tease :: Door -> Door -> Double -> [Door]
-tease h p real = uniform (doors \/ [h,p]) real
+tease h p = uniform (doors \/ [h,p])
 
 -- Switching the original choice
 switch :: Door -> Door -> [Door]
@@ -75,15 +76,15 @@ play g strategy = do
   s <- strategy p t
   return (s == h)
 
-{-
 -- Cartesian Product
 cp :: [a] -> [b] -> [(a,b)]
 cp x y = [(a,b) | a <- x, b <- y]
 
-play2 :: (Door -> Door -> [Door]) -> [Bool]
-play2 strategy = do
-  (h,p) <- uniform (cp doors doors)
-  t <- tease h p
+play2 :: StdGen -> (Door -> Door -> [Door]) -> [Bool]
+play2 g strategy = do
+  let (r1,g1) = random g
+      (r2,_)  = random g1
+  (h,p) <- uniform (cp doors doors) r1
+  t <- tease h p r2
   s <- strategy p t
   return (s == h)
--}
