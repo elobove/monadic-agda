4398900059767cadc5c483f6a72b205f1491fd5f
Elisabet <elilobo.v@gmail.com>
2012-11-11 16:20:39 -0500

    Addingthe main function

--- Stateful.hs
+++ Stateful.hs
@@ -1,18 +1,19 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE FlexibleContexts #-}
 module Stateful where
 
 import NonDet
-import Control.Monad.State hiding (guard)
-{-
-class Monad m => MonadState s m | m -> s where
-  get :: m s
-  put :: s -> m ()
--}
+import Control.Monad.State
+
+-- STATEFUL COMPUTATIONS
+main :: IO ()
+main = do
+  putStrLn "Insert the positions"
+  cr <- getLine
+  print (runState (safe2 $ read cr) ([],[]))
 
 type Square a = (a,a)
 
+-- N queens puzzle
 -- Purely functional implementation
 square :: (a -> b) -> Square a -> Square b
 square f (a,b) = (f a, f b)
@@ -40,7 +41,7 @@ queens n = do
   guard (fst(safe1 empty (place n rs)))
   return rs
 
-place n rs = zip [1..n] rs
+place n = zip [1..n]
 empty = ([],[])
 
 -- Statefully implementation
@@ -52,14 +53,11 @@ start2 = return True
 
 step2 :: MonadState (Square [Int]) m => Square Int -> m Bool -> m Bool
 step2 cr k = do
-  b' <- k
+  b'  <- k
   uds <- get
   let (b,uds') = test cr uds
   put uds'
   return (b && b')
 
--- run :: MonadState (Square [Int]) m => m Bool -> Square [Int] ->
---       (Bool, Square [Int])
-
-test10 :: [Square Int] -> Square [Int] -> Bool
-test10 crs updowns = runState (safe2 crs) updowns == safe1 updowns crs
+test2 :: [Square Int] -> Square [Int] -> Bool
+test2 crs updowns = runState (safe2 crs) updowns == safe1 updowns crs
